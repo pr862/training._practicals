cb2683c44fa400b9c04eae49942c0a30b31ba2d8
Princy Vithani <princyvithani09@gmail.com>
2026-01-19 11:08:56 +0530

    feat: use Tailwind CDN and remove CLI dependencies

--- practical-03/swiper.js
+++ practical-03/swiper.js
@@ -10,10 +10,6 @@ const teamSwiper = new Swiper(".teamSwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    bulletClass:
-      "swiper-pagination-bullet w-3 h-3 sm:w-4 sm:h-4 bg-white rounded-sm border border-gray-300 opacity-100 transition-all duration-300",
-    bulletActiveClass:
-      "swiper-pagination-bullet-active bg-red-300"
   },
 
   breakpoints: {
