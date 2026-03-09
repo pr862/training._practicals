a5d9a5c35a2b57eb75f7c520a2207e7f2bb50de7
pr862 <princyvithani09@gmail.com>
2026-03-09 19:11:42 +0530

    replace createWebHashHistory with createWebHistory for clean URLs

--- practical-17/frontend/src/router/index.ts
+++ practical-17/frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore } from '@/store';
 
 import AdminLayout from '@/layouts/AdminLayout.vue';
@@ -60,7 +60,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
