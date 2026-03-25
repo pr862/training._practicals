3d23e63f62931836d66afadf8552a7068df0c271
Princy Vithani <princyvithani09@gmail.com>
2026-03-25 16:10:19 +0530

    change routes

--- practical-21/src/services/favouriteService.ts
+++ practical-21/src/services/favouriteService.ts
@@ -2,9 +2,9 @@ import api from './api';
 import type { ApiResponse, FavouriteTrack } from '../types/api';
 
 export const favouriteService = {
-  getAll: (userId: number) => api.get<ApiResponse<FavouriteTrack[]>>(`/users/favourites/${userId}`),
+  getAll: (userId: number) => api.get<ApiResponse<FavouriteTrack[]>>(`/user/favourites/${userId}`),
   add: (trackId:  number) =>
-    api.post<ApiResponse<FavouriteTrack>>(`/users/favourites/${trackId}`),
+    api.post<ApiResponse<FavouriteTrack>>(`/user/favourites/${trackId}`),
   removeByTrackId: (trackId:number) =>
-    api.delete<ApiResponse<FavouriteTrack>>(`/users/favourites/${trackId}`),
+    api.delete<ApiResponse<FavouriteTrack>>(`/user/favourites/${trackId}`),
 };
